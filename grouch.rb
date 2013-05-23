ac4bdccf2cd5db786d4eaccc493b265cabb862df
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-05-22 20:08:39 -0700

    initial

---
+++ grouch.rb
@@ -0,0 +1,29 @@
+require './couch.rb'
+
+module Grouch
+
+	class Server < Couch::Server
+		#Creates an instance of with basic CouchDB commands and .
+		def initialize host, port, options = nil
+			@cdb_server = Couch::Server.new host, port, options
+		end
+
+		#Generates design/design.json from files design/type_viewname.js 
+		def generate_design
+			#TODO implement
+		end
+
+		#Uploads design.json to database db_name
+		def upload_design db_name
+			#TODO implement
+
+			# Pseudocode
+			#doc = read /design.json
+			#cdb_server.put db_name/_design/grouch/ doc 
+		end
+	end
+end
+
+server = Grouch::Server.new "localhost", 5984
+server.generate_design
+server.upload_design ARGV[0]
